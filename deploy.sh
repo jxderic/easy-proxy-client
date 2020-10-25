9dbd0ba760232b44eba505d3eae09de214b0c304
金晓东 <eric4336355@126.com>
2020-10-25 11:51:40 +0800

    fix: 修改自动化脚本

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "deploying"
 yarn config set registry https://registry.npm.taobao.org/
 yarn install
 yarn run build
-scp -r ./dist/* /usr/share/nginx/html/proxy
+scp -r ./build/* /usr/share/nginx/html/proxy
 echo "deploy done"
