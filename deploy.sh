5c181f15aaddce11b299862302d2bd1357a9fff5
金晓东 <eric4336355@126.com>
2020-10-29 21:37:13 +0800

    fix: 修改自动化命令

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "deploying"
 yarn config set registry https://registry.npm.taobao.org/
+yarn config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass -g
 yarn install
 yarn run build
 scp -r ./build/* /usr/share/nginx/html/proxy
